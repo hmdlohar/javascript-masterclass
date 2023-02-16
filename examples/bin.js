35de5017c3cfb4eca015620f10ab34e24aa8e4e9
Hamid Lohar <hmdlohar@gmail.com>
2023-02-16 21:53:11 +0530

    html

--- examples/bin.js
+++ examples/bin.js
@@ -1,20 +1,13 @@
-function bin2dec(bin) {
-  let dec = 0;
-  for (let i = 0; i < bin.length; i++) {
-    let position = bin.length - i - 1;
-    if (bin[position] === "1") {
-      dec += power(2, i);
-    }
-  }
-  return dec;
-}
+var button = document.getElementById("myButton");
+button.addEventListener("click", function () {
+  alert("Button was clicked!");
+});
 
-function power(n, p) {
-  let ans = 1;
-  for (let i = 0; i < p; i++) {
-    ans *= n;
-  }
-  return ans;
-}
 
-console.log(bin2dec("10101"));
+<button onclick="myFunction()">Click me</button>
+
+
+var button = document.getElementById("myButton");
+object.onclick = function(){
+  // YOur code
+};
